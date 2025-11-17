ce64a53d49fa0fd73b297615137fe72e5b1380b7
Ahmed Essam <ahmedessam14xb@gmail.com>
2025-11-17 03:17:16 +0200

    add Games solution

---
+++ codeforces/Games.cpp
@@ -0,0 +1,27 @@
+// problem name: Games
+// problem link: https://codeforces.com/contest/268/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a1, a2, a3, a4;
+    cin >> a1 >> a2 >> a3 >> a4;
+    int ans = 0;
+    string s; cin >> s;
+    for(char c : s){
+        switch (c){
+            case '1':
+                ans += a1;
+                break;
+            case '2':
+                ans += a2;
+                break;
+            case '3':
+                ans += a3;
+                break;
+            case '4':
+                ans += a4;
+        }
+    }
+    printf("%d\n",ans);
+}
