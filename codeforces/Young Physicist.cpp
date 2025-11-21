109f1a0ffaaf47835ec5f3c2526406a2993acfa0
Ahmed Essam <ahmedessam14xb@gmail.com>
2025-11-21 13:48:26 +0200

    add Young Physicist solution

---
+++ codeforces/Young Physicist.cpp
@@ -0,0 +1,46 @@
+// problem name: Young Physicist
+// problem link: https://codeforces.com/contest/69/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define intmaxx INTMAX_MAX
+#define intminn INTMAX_MIN
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define vc vector<int>
+#define vci vector<int>
+#define vcll vector<long long>
+#define vcstr vector<string>
+#define ring(a, b) for (int i = a; i < b; i++)
+
+int main() {
+        int n ; cin >> n;
+        int sum_x = 0 , sum_y = 0, sum_z = 0;
+        while(n--){
+        int x, y, z; 
+                cin >> x >> y >> z;
+                sum_x += x; sum_y += y;
+                sum_z += z;
+        }
+        if(!sum_x && !sum_y && !sum_z){
+                printf("YES");
+        }
+        else 
+                printf("NO\n");
+}
+
+/*
+void solve(){
+        int x, y; cin >> x >> y;
+        cout << x + y << '\n';
+        cout << x * y << '\n';
+}
+int main() {
+        int t; cin >> t;
+        while(t--){
+                solve();
+        }
+        return 0;
+}*/
