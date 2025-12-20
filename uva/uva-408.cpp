3921b9a9c24f601badab407561e3d7e2689eeb6c
Ahmed Essam <ahmedessam14xb@gmail.com>
2025-12-20 11:19:27 +0200

    add uva-408 solution

---
+++ uva/uva-408.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define intmaxx INTMAX_MAX
+#define intminn INTMAX_MIN
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define vc vector<int>
+#define vci vector<int>
+#define vcll vector<long long>
+#define vcstr vector<string>
+#define ring(a, b) for (int i = a; i < b; i++)
+
+int main() {
+  int step, mod;
+  while (cin >> step) {
+    cin >> mod;
+    set<int> seeds;
+    seeds.insert(0);
+    int prev_seed = 0, cur_seed;
+    while (true) {
+      cur_seed = ((prev_seed % mod) + (step % mod)) % mod;
+      if (seeds.count(cur_seed))
+        break;
+      seeds.insert(cur_seed);
+      prev_seed = cur_seed;
+    }
+    string ans = "Bad Choice";
+    if (seeds.size() == mod)
+      ans = "Good Choice";
+
+    cout << right << setw(10) << step << setw(10) << mod << "    " << left
+         << ans << '\n';
+    cout << '\n';
+  }
+}
