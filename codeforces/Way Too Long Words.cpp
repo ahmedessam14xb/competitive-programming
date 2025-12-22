7db2d14455dce6c4334ed978748ed01659e302a4
Ahmed Essam <ahmedessam14xb@gmail.com>
2025-12-22 16:49:37 +0200

    add Way Too Long Words solution

---
+++ codeforces/Way Too Long Words.cpp
@@ -0,0 +1,17 @@
+// problem name: Way Too Long Words
+// problem link: https://codeforces.com/contest/71/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	while (n--) {
+		string s;
+		cin >> s;
+		if (s.size() > 10) {
+			cout << s[0] << s.size() - 2 << s[s.size() - 1] << '\n';
+		} else
+			cout << s << '\n';
+	}
+}
