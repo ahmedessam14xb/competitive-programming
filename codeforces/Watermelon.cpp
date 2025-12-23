33c9f0b2a62c66018cfabc9f08cf006897fa9164
Ahmed Essam <ahmedessam14xb@gmail.com>
2025-12-23 20:32:25 +0200

    add Watermelon solution

---
+++ codeforces/Watermelon.cpp
@@ -0,0 +1,34 @@
+// problem name: Watermelon
+// problem link: https://codeforces.com/problemset/problem/4/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int nxti() {
+  int x;
+  cin >> x;
+  return x;
+}
+string nxts() {
+  string x;
+  cin >> x;
+  return x;
+}
+#define intmaxx INTMAX_MAX
+#define intminn INTMAX_MIN
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define vcstr vector<string>
+#define ring(a, b) for (int i = a; i < b; i++)
+
+int main() {
+  int w = nxti();
+  if (w & 1 or w == 2)
+    cout << "NO\n";
+  else
+    cout << "YES\n";
+}
