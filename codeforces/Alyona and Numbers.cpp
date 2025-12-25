84278249a048705c9726adef6ef35c82fba9b53a
Ahmed Essam <ahmedessam14xb@gmail.com>
2025-12-25 13:14:21 +0200

    add Alyona and Numbers solution

---
+++ codeforces/Alyona and Numbers.cpp
@@ -0,0 +1,36 @@
+// problem name: Alyona and Numbers
+// problem link: https://codeforces.com/contest/682/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int nxti() {
+  int x;
+  cin >> x;
+  return x;
+}
+string nxts() {
+  string x;
+  cin >> x;
+  return x;
+}
+#define intmaxx INTMAX_MAX
+#define intminn INTMAX_MIN
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define vcstr vector<string>
+#define ring(a, b) for (int i = a; i < b; i++)
+
+int main() {
+  ll n, m;
+  cin >> n >> m;
+  ll ans = (n / 5) * (m / 5) * 5 + ((n % 5) * (m / 5)) + ((n / 5) * (m % 5));
+  if(n % 5 + m % 5 > 4){
+	ans += (n % 5 + m % 5) - 4;
+  }
+  cout << ans << '\n';
+}
