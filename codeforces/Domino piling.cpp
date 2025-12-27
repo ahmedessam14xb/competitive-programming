d3c4bd689a1f6de2f89e78714989f1b5947220c5
Ahmed Essam <ahmedessam14xb@gmail.com>
2025-12-27 07:48:24 +0200

    add Domino piling solution

---
+++ codeforces/Domino piling.cpp
@@ -0,0 +1,33 @@
+// problem name: Domino piling
+// problem link: https://codeforces.com/problemset/problem/50/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int nxti() {
+  int x;
+  cin >> x;
+  return x;
+}
+string nxts() {
+  string x;
+  cin >> x;
+  return x;
+}
+#define intmaxx INTMAX_MAX
+#define intminn INTMAX_MIN
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define vcstr vector<string>
+#define ring(a, b) for (int i = a; i < b; i++)
+
+int main() {
+  ll m, n;
+  cin >> m >> n;
+  ll ans = m * n / 2;
+  cout << ans << '\n';
+}
