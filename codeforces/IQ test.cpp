02181599e86a50343a8559f2dbed0ad099baf466
Ahmed Essam <ahmedessam14xb@gmail.com>
2025-12-30 21:15:14 +0200

    add IQ test solution

--- codeforces/IQ test.cpp
+++ codeforces/IQ test.cpp
@@ -1,17 +1,17 @@
 // problem name: IQ test
-// problem link: https://codeforces.com/problemset/problem/25/A
+// problem link: https://codeforces.com/contest/25/problem/A
 #include <bits/stdc++.h>
 using namespace std;
 
 int nxti() {
-  int x;
-  cin >> x;
-  return x;
+	int x;
+	cin >> x;
+	return x;
 }
 string nxts() {
-  string x;
-  cin >> x;
-  return x;
+	string x;
+	cin >> x;
+	return x;
 }
 #define intmaxx INTMAX_MAX
 #define intminn INTMAX_MIN
@@ -26,21 +26,21 @@ string nxts() {
 #define ring(a, b) for (int i = a; i < b; i++)
 
 int main() {
-  int n;
-  cin >> n;
-  int arr[n];
-  int odd = 0, even = 0;
-  int odd_pos = -1, even_pos = -1;
-  ring(0, n) {
-    cin >> arr[i];
-    if (arr[i] % 2)
-      odd++, odd_pos = i + 1;
-    else
-      even++, even_pos = i + 1;
-  }
-  if (even > odd)
-    cout << odd_pos;
-  else
-    cout << even_pos;
-  cout << '\n';
+	int n;
+	cin >> n;
+	int arr[n];
+	int odd = 0, even = 0;
+	int odd_pos = -1, even_pos = -1;
+	ring(0, n) {
+		cin >> arr[i];
+		if (arr[i] % 2)
+			odd++, odd_pos = i + 1;
+		else
+			even++, even_pos = i + 1;
+	}
+	if (even > odd)
+		cout << odd_pos;
+	else
+		cout << even_pos;
+	cout << '\n';
 }
