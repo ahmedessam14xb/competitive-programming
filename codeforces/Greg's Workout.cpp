c166441e1d709a04d2dfccef8fe1f75264256aa0
Ahmed Essam <ahmedessam14xb@gmail.com>
2025-12-31 16:12:08 +0200

    add Greg's Workout solution

---
+++ codeforces/Greg's Workout.cpp
@@ -0,0 +1,30 @@
+// problem name: Greg's Workout
+// problem link: https://codeforces.com/contest/255/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int nxti() {
+  int x;
+  cin >> x;
+  return x;
+}
+string nxts() {
+  string x;
+  cin >> x;
+  return x;
+}
+#define intmaxx INTMAX_MAX
+#define intminn INTMAX_MIN
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define vcstr vector<string>
+#define ring(a, b) for (int i = a; i < b; i++)
+
+void solve() {}
+
+int main() { solve(); }
