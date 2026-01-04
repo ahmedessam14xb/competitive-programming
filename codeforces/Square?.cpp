a3e53fe1ec4aedbbe2b5053d1e73d3c720e61375
Ahmed Essam <ahmedessam14xb@gmail.com>
2026-01-04 17:26:26 +0200

    add Square? solution

---
+++ codeforces/Square?.cpp
@@ -0,0 +1,36 @@
+// problem name: Square?
+// problem link: https://codeforces.com/problemset/problem/2167/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define intmaxx INTMAX_MAX
+#define intminn INTMAX_MIN
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(a, b) for (int i = a; i < b; i++)
+#define sz(n) (n).size()
+#define ln puts("");
+
+void solve() {
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+  if (a == b and b == c and c == d)
+    cout << "YES\n";
+  else
+    cout << "NO\n";
+}
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--)
+    solve();
+}
