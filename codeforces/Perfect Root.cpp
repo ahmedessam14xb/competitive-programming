99074f28967e2579839e6134fd59ead123c91ebf
Ahmed Essam <ahmedessam14xb@gmail.com>
2026-01-23 14:09:35 +0200

    add Perfect Root solution

---
+++ codeforces/Perfect Root.cpp
@@ -0,0 +1,33 @@
+// problem name: Perfect Root
+// problem link: https://codeforces.com/problemset/problem/2185/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define intmaxx INTMAX_MAX
+#define intminn INTMAX_MIN
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(a, b) for (int i = a; i < b; i++)
+#define sz(n) (n).size()
+#define ln puts("");
+
+void solve() {
+    int n ; cin >> n;
+    ring(1,n+1){
+        cout << i << " ";
+    }
+    ln
+}
+
+int main() { 
+    int t; cin >> t;
+    while(t--)
+    solve(); }
