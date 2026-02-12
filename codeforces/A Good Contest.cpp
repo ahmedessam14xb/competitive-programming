a7cd15eb06bac8377d5113b012bd0ae7f5480364
Ahmed Essam <ahmedessam14xb@gmail.com>
2026-02-12 16:46:32 +0200

    add A Good Contest solution

---
+++ codeforces/A Good Contest.cpp
@@ -0,0 +1,40 @@
+// problem name: A Good Contest
+// problem link: https://codeforces.com/contest/681/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define intmaxx INTMAX_MAX
+#define intminn INTMAX_MIN
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(a, b) for (int i = a; i < b; i++)
+#define sz(n) (n).size()
+#define ln puts("");
+
+void solve() {
+  int n;
+  cin >> n;
+  bool found = false;
+  ring(0, n) {
+    string s;
+    int before, after;
+    cin >> s >> before >> after;
+    if (before >= 2400 and (after - before) >= 1) {
+      found = true;
+    }
+  }
+  if (found) {
+    printf("YES\n");
+  } else
+    printf("NO\n");
+}
+
+int main() { solve(); }
