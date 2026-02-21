f2460f3a83c8a497ba39214186a795db6cbecf8e
Ahmed Essam <ahmedessam14xb@gmail.com>
2026-02-21 12:05:21 +0200

    add vJudge-The Drunk Jailer solution

---
+++ vJudge/The Drunk Jailer.cpp
@@ -0,0 +1,32 @@
+// problem name: The Drunk Jailer
+// problem link: https://vjudge.net/problem/OpenJ_Bailian-1218
+#include <bits/stdc++.h>
+using namespace std;
+
+#define intmaxx INTMAX_MAX
+#define intminn INTMAX_MIN
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(a, b) for (int i = a; i < b; i++)
+#define sz(n) (n).size()
+#define ln puts("");
+
+void solve() {
+	ll a;
+	cin >> a;
+	cout << (int)sqrt(a) << '\n';
+}
+
+int main() {
+	int n;
+	cin >> n;
+	while (n--) solve();
+}
