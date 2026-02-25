e10e626e08395b4d795a58e7f161930bfd6a5da9
Ahmed Essam <ahmedessam14xb@gmail.com>
2026-02-25 20:49:00 +0200

    add Keyboard solution

--- codeforces/Keyboard.cpp
+++ codeforces/Keyboard.cpp
@@ -1,5 +1,5 @@
 // problem name: Keyboard
-// problem link: https://codeforces.com/contest/474/problem/A
+// problem link: https://codeforces.com/contest/88/problem/B
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -9,27 +9,67 @@ using namespace std;
 #define ull unsigned long long
 #define uint unsigned int
 #define pb push_back
-#define vc vector<int>
+#define all(x) (x).begin(), (x).end()
 #define vci vector<int>
 #define vcll vector<long long>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
 #define vcstr vector<string>
 #define ring(a, b) for (int i = a; i < b; i++)
+#define sz(n) (n).size()
+#define ln puts("");
 
-int main() {
-  string arr[3] = {"qwertyuiop", "asdfghjkl;", "zxcvbnm,./"};
-  char dir;
-  cin >> dir;
-  string s;
-  cin >> s;
-  for (int i = 0; i < s.size(); i++) {
-    for (int word = 0; word < 3; word++) {
-      for (int ch = 0; ch < arr[word].size(); ch++) {
-        if (arr[word][ch] == s[i])
-          if (dir == 'R')
-            cout << arr[word][ch - 1];
-          else
-            cout << arr[word][ch + 1];
-      }
+void solve() {
+  int n, m, x;
+  cin >> n >> m >> x;
+  map<char, vector<pair<int, int>>> keyboard;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      char c;
+      cin >> c;
+      if (c == 'S')
+        c = '0';
+      keyboard[c].push_back({i, j});
     }
   }
+  int q;
+  cin >> q;
+  string s;
+  cin >> s;
+  int cnt = 0;
+  map<char, double> visited_distance;
+  for (char c : s) {
+    if (keyboard.count((char)tolower(c)) == 0) {
+      cout << -1 << '\n';
+      return;
+    }
+    if (isupper(c)) {
+      if (sz(keyboard['0']) == 0) {
+        cout << -1 << '\n';
+        return;
+      }
+      double min_distance = 1000.0;
+      if (visited_distance.count((char)tolower(c)))
+        min_distance = visited_distance[(char)tolower(c)];
+      else {
+        vector<pair<int, int>> charArr = keyboard[(char)tolower(c)];
+        for (int i = 0; i < charArr.size(); i++) {
+          for (int j = 0; j < keyboard['0'].size(); j++) {
+            double distance =
+                sqrt((keyboard['0'][j].first - charArr[i].first) *
+                         (keyboard['0'][j].first - charArr[i].first) +
+                     (keyboard['0'][j].second - charArr[i].second) *
+                         (keyboard['0'][j].second - charArr[i].second));
+            min_distance = min(min_distance, distance);
+          }
+        }
+        visited_distance[(char)tolower(c)] = min_distance;
+      }
+      if (min_distance > x)
+        cnt++;
+    }
+  }
+  printf("%d\n", cnt);
 }
+
+int main() { solve(); }
