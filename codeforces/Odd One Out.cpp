d13008396f3aabf21605db6c5b660947c5ef2813
Ahmed Essam <ahmedessam14xb@gmail.com>
2026-02-27 10:40:20 +0200

    add Odd One Out solution

---
+++ codeforces/Odd One Out.cpp
@@ -0,0 +1,35 @@
+// problem name: Odd One Out
+// problem link: https://codeforces.com/problemset/problem/1915/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define intmaxx INTMAX_MAX
+#define intminn INTMAX_MIN
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(a, b) for (int i = a; i < b; i++)
+#define sz(n) (n).size()
+#define ln puts("");
+
+void solve() {
+  int a, b, c; cin >> a >> b >> c;
+  if(a == b) cout << c;
+  else if (b == c) cout << a ;
+  else cout << b;
+  cout << '\n';
+}
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--)
+    solve();
+}
