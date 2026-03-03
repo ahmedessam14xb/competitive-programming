bcc5f1dc4fcad0a3a510064b7039efe5f5411d87
Ahmed Essam <ahmedessam14xb@gmail.com>
2026-03-03 18:51:48 +0200

    add Divisibility Problem solution

---
+++ codeforces/Divisibility Problem.cpp
@@ -0,0 +1,35 @@
+// problem name: Divisibility Problem
+// problem link: https://codeforces.com/problemset/problem/1328/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define intmaxx INTMAX_MAX
+#define intminn INTMAX_MIN
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(i, a, b) for (int i = a; i < b; i++)
+#define ring0(i, n) ring(i, 0, n)
+#define dring(i, j, a, b) ring0(i, a) ring0(j, b)
+#define sz(n) (n).size()
+#define ln puts("");
+
+void solve() {
+    int a, b; cin >> a >> b;
+    if(a % b == 0) cout << 0 << '\n';
+    else {
+        cout << b - (a % b) << '\n';
+    }
+}
+
+int main() {
+    int t; cin >> t;
+    while(t--) solve();
+}
