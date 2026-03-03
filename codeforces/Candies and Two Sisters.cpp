e8b794b7edecfd05a6b4743a40d34c6123575286
Ahmed Essam <ahmedessam14xb@gmail.com>
2026-03-03 19:08:17 +0200

    add Candies and Two Sisters solution

---
+++ codeforces/Candies and Two Sisters.cpp
@@ -0,0 +1,34 @@
+// problem name: Candies and Two Sisters
+// problem link: https://codeforces.com/problemset/problem/1335/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define intmaxx INTMAX_MAX
+#define intminn INTMAX_MIN
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(i, a, b) for (int i = a; i < b; i++)
+#define ring0(i, n) ring(i, 0, n)
+#define dring(i, j, a, b) ring0(i, a) ring0(j, b)
+#define sz(n) (n).size()
+#define ln puts("");
+
+void solve() {
+    int n ; cin >> n;
+    cout << (n-1) /2 << '\n';
+}
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--)
+    solve();
+}
