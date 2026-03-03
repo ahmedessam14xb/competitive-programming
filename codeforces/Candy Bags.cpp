5ca821e7e72843004ea4b26d074268d207babc85
Ahmed Essam <ahmedessam14xb@gmail.com>
2026-03-03 06:04:35 +0200

    add Candy Bags solution

---
+++ codeforces/Candy Bags.cpp
@@ -0,0 +1,32 @@
+// problem name: Candy Bags
+// problem link: https://codeforces.com/problemset/problem/334/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define intmaxx INTMAX_MAX
+#define intminn INTMAX_MIN
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(i, a, b) for (int i = a; i < b; i++)
+#define ring0(i, n) ring(i, 0, n)
+#define dring(i, j, a, b) ring0(i, a) ring0(j, b)
+#define sz(n) (n).size()
+#define ln puts("");
+
+void solve() {
+    int n; cin >> n;
+    int mx = n*n + 1;
+    ring(i, 1, mx/2 + 1){
+        cout << i << " " << mx - i << '\n';
+    }
+}
+
+int main() { solve(); }
