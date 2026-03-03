9fc25b084aa1475d6fb71628660df67a8eb2c9e1
Ahmed Essam <ahmedessam14xb@gmail.com>
2026-03-03 18:47:35 +0200

    add In Search of an Easy Problem solution

---
+++ codeforces/In Search of an Easy Problem.cpp
@@ -0,0 +1,35 @@
+// problem name: In Search of an Easy Problem
+// problem link: https://codeforces.com/problemset/problem/1030/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define intmaxx INTMAX_MAX
+#define intminn INTMAX_MIN
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(i, a, b) for (int i = a; i < b; i++)
+#define ring0(i, n) ring(i, 0, n)
+#define dring(i, j, a, b) ring0(i, a) ring0(j, b)
+#define sz(n) (n).size()
+#define ln puts("");
+
+void solve() {
+    int n; cin >> n;
+    int sum = 0;
+    ring(i, 0, n){
+        int x; cin >> x;
+        sum += x;
+    }
+    if(sum > 0) cout << "HARD\n";
+    else cout << "EASY\n";
+}
+
+int main() { solve(); }
