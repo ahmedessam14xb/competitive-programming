7c734ab0e32fcd83fd80e47d3948ba287b4a1a44
Ahmed Essam <ahmedessam14xb@gmail.com>
2026-03-04 02:59:23 +0200

    add Array Coloring solution

---
+++ codeforces/Array Coloring.cpp
@@ -0,0 +1,40 @@
+// problem name: Array Coloring
+// problem link: https://codeforces.com/problemset/problem/1857/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define intmaxx INTMAX_MAX
+#define intminn INTMAX_MIN
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(i, a, b) for (int i = a; i < b; i++)
+#define ring0(i, n) ring(i, 0, n)
+#define dring(i, j, a, b) ring0(i, a) ring0(j, b)
+#define sz(n) (n).size()
+#define ln puts("");
+
+void solve() {
+    int n; cin >> n;
+    int odds = 0;
+    while(n--){
+        int x; cin >> x;
+        if(x & 1) odds++;
+    }
+    if(!(odds & 1)) cout << "YES\n";
+    else cout << "NO\n";
+}
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--)
+    solve();
+}
