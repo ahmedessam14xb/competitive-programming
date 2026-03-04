ec20cd2ef878fd2aa05d75cdbac355caf84bf502
Ahmed Essam <ahmedessam14xb@gmail.com>
2026-03-04 02:14:20 +0200

    add Game with Integers solution

---
+++ codeforces/Game with Integers.cpp
@@ -0,0 +1,36 @@
+// problem name: Game with Integers
+// problem link: https://codeforces.com/problemset/problem/1899/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define intmaxx INTMAX_MAX
+#define intminn INTMAX_MIN
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(i, a, b) for (int i = a; i < b; i++)
+#define ring0(i, n) ring(i, 0, n)
+#define dring(i, j, a, b) ring0(i, a) ring0(j, b)
+#define sz(n) (n).size()
+#define ln puts("");
+
+void solve() {
+    int n ; cin >> n;
+    if(n % 3 == 0){
+        cout << "Second\n";
+    }
+    else cout << "First\n";
+}
+
+int main() {
+    int t; cin >> t;
+    while(t--)
+    solve();
+}
