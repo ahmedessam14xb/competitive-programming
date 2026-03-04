b2bff40dffa3667eec01f32c0e1d90304927ade0
Ahmed Essam <ahmedessam14xb@gmail.com>
2026-03-04 03:25:56 +0200

    add To My Critics solution

---
+++ codeforces/To My Critics.cpp
@@ -0,0 +1,39 @@
+// problem name: To My Critics
+// problem link: https://codeforces.com/problemset/problem/1850/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define intmaxx INTMAX_MAX
+#define intminn INTMAX_MIN
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vci vector<int>
+#define vcll vector<long long>
+#define unmapii unordered_map<int, int>
+#define unmapsi unordered_map<string, int>
+#define vcstr vector<string>
+#define ring(i, a, b) for (int i = a; i < b; i++)
+#define ring0(i, n) ring(i, 0, n)
+#define dring(i, j, a, b) ring0(i, a) ring0(j, b)
+#define sz(n) (n).size()
+#define ln puts("");
+
+void solve() {
+  int arr[3];
+  ring(i, 0, 3){
+    cin >> arr[i]; 
+  }
+  sort(arr, arr+3, greater<int>());
+  if(arr[0] + arr[1] >= 10) cout << "YES\n";
+  else cout << "NO\n";
+}
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--)
+    solve();
+}
